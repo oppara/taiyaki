56ee025f9f118d9f65c056577eb2dee99c2800f0
oppara <oppara@oppara.tv>
2017-10-13 00:15:11 +0900

    step 0

--- TaiyakiTest.php
+++ TaiyakiTest.php
@@ -3,7 +3,20 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use PHPUnit\Framework\TestCase;
 
+class Taiyaki
+{
+
+}
+
 class TaiyakiTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function taiyaki()
+    {
+        $this->assertInstanceOf(Taiyaki::class, new Taiyaki);
+    }
+
 }
 
