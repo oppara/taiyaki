e9f914666bd42904d5f6da27bd6b5a197345d353
oppara <oppara@oppara.tv>
2017-10-13 00:58:58 +0900

    step 4

--- TaiyakiTest.php
+++ TaiyakiTest.php
@@ -7,11 +7,25 @@ class Taiyaki
 {
     private $anko;
     private $size;
+    private $price;
+    private $base_price = 100;
 
     public function __construct($anko, $size)
     {
         $this->anko = $anko;
         $this->size = $size;
+        $this->setPrice();
+    }
+
+    private function setPrice()
+    {
+        $this->price = $this->base_price;
+        if ($this->anko == '白あん') {
+            $this->price += 30;
+        }
+        if ($this->size == '大きめ') {
+            $this->price += 50;
+        }
     }
 
     public function __get($name)
@@ -23,7 +37,7 @@ class Taiyaki
 
     public function __toString()
     {
-        return sprintf('あんこ: %s, 大きさ: %s', $this->anko, $this->size);
+        return sprintf('あんこ: %s, 大きさ: %s, %d円', $this->anko, $this->size, $this->price);
     }
 
 }
@@ -34,19 +48,20 @@ class TaiyakiTest extends TestCase
      * @test
      * @dataProvider taiyakiProvider
      */
-    public function taiyaki($anko, $size, $str)
+    public function taiyaki($anko, $size, $price, $str)
     {
         $taiyaki = new Taiyaki($anko, $size);
         $this->assertSame($anko, $taiyaki->anko);
         $this->assertSame($size, $taiyaki->size);
+        $this->assertSame($price, $taiyaki->price);
         $this->assertSame($str, (string) $taiyaki);
     }
 
     public function taiyakiProvider()
     {
         return [
-            ['あずき', 'ふつう', 'あんこ: あずき, 大きさ: ふつう'],
-            ['白あん', '大きめ', 'あんこ: 白あん, 大きさ: 大きめ'],
+            ['あずき', 'ふつう', 100, 'あんこ: あずき, 大きさ: ふつう, 100円'],
+            ['白あん', '大きめ', 180, 'あんこ: 白あん, 大きさ: 大きめ, 180円'],
         ];
     }
 }
